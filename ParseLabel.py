f2f8360e66cca3ee4e26d7cd30df9d1216de5706
brandon <blhguo@gmail.com>
2018-10-13 18:26:24 -0400

    built daily reminders to take medication, repeating up until the refill day. Fixed bug with multiple copies of a reminder in one day

--- ParseLabel.py
+++ ParseLabel.py
@@ -33,7 +33,7 @@ def createReminder(service, uri):
         'timeZone': 'America/New_York',
       },
       'recurrence': [
-        'RRULE:FREQ=DAILY;COUNT=2'
+        'RRULE:FREQ=DAILY;COUNT=1'
       ],
       'reminders': {
         'useDefault': False,
@@ -44,5 +44,43 @@ def createReminder(service, uri):
       },
     }
     event = service.events().insert(calendarId='primary', body=event).execute()
+    for i in range(int(y)):
+        print("make")
+        temp = createR(now, i)
+        temp = service.events().insert(calendarId='primary', body=temp).execute()
+
+    
     print("finished")
+
+def createR(myInput, MyIncre):
+    td = timedelta(days=MyIncre)
+    mydate = myInput + td
+    Mydate = mydate.isoformat()
+    td1 = timedelta(hours=1)
+    mydate1 = mydate + td1
+    Mydate1 = mydate1.isoformat()
+    event = {
+      'summary': 'Daily reminder',
+      'description': 'A chance to hear more about Google\'s developer products.',
+      'start': {
+        'dateTime': Mydate,
+        'timeZone': 'America/New_York',
+      },
+      'end': {
+        'dateTime': Mydate1,
+        'timeZone': 'America/New_York',
+      },
+      'recurrence': [
+        'RRULE:FREQ=DAILY;COUNT=1'
+      ],
+      'reminders': {
+        'useDefault': False,
+        'overrides': [
+          {'method': 'email', 'minutes': 24 * 60},
+          {'method': 'popup', 'minutes': 10},
+        ],
+      },
+    }
+    return event
+
 
